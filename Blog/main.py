628cf282e5352b98e3a029ed70219fce5d956ee2
Shaikh Imran <imranshkh283@gmail.com>
2025-06-15 00:32:05 +0530

    declare schema for blog

--- Blog/main.py
+++ Blog/main.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from . import schemas
 app = FastAPI()
 
-
-class Blog(BaseModel):
-    title: str
-    body: str
 
 @app.get("/")
 def root():
     return {"root": "FastApi is working on port 8000"}
 
 @app.post("/blog")
-def create_blog(request: Blog):
+def create_blog(request: schemas.Blog):
     return request
