c2c7b19d9855a3b7adcd7d8049617f798d510cda
Shaikh Imran <imranshkh283@gmail.com>
2025-06-15 00:28:35 +0530

    blog post api test

--- Blog/main.py
+++ Blog/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
 
 @app.get("/")
 def root():
     return {"root": "FastApi is working on port 8000"}
+
+@app.post("/blog")
+def create_blog(request: Blog):
+    return request
