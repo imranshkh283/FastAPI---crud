effe0d047594465cd4f491e0d2980ad61477a096
Shaikh Imran <imranshkh283@gmail.com>
2025-06-14 23:26:58 +0530

    debug mode

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
+port = 9000
+@app.get("/")
+def root():
+    return {"root": "FastApi is working on port 9000"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=port)
